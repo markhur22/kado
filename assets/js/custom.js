10da4aa6df869e7e383ddc66c3dd245af0e8023e
sahilnarula90 <110301889+sahilnarula90@users.noreply.github.com>
2022-09-10 14:32:36 +0530

    swap changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -685,7 +685,42 @@ $('.card_view_slide_main').slick({
 	  ]
 });
 
-
+$('.swap_slider').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  adaptiveHeight: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
 
 $('#gift_sent_pop').on('shown.bs.modal', function (e) {
   $('.card_view_slide_main').slick('setPosition');
